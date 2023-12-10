c5595e25d51236a5df32bb41d90dfb4890a0e079
jxwu <1463009704@qq.com>
2023-12-10 15:46:54 +0800

    explain the blend in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -57,11 +57,29 @@ int main(void)
 		};
 
 		/*
-		* enable blend!
-		* i dont find the problem the tutorial have ,so just leave it as a tip!
-		*/ 
+		what is blending£¿ how  do we render something that partially or fully transparent!
+
+		if we want to draw two square,one is read,one is blue,so what is the color of the intersection of this two square? 
+		the square color of top ,or blend to purple?
+
+		we have three ways to control blending
+		1. glEnable(GL_BLEND); glDisable(GL_BLEND);
+		2. glBlendFunc(src=GL_ONE, dest=GL_ZERO),src is factor of color output from fragment shader
+		and dest is factor to buffer our fragment shader is drawing to .GL_ONE and GL_ZERO is default.
+		3. glBlendEquation(mode = GL_FUNC_ADD); mode = how we combine the src and dest color!
+		*/
 		GLCallV(glEnable(GL_BLEND));
-		GLCallV(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		/*
+		the follow function can be explain as :
+		if src alpha = 0.1 -----> GL_SRC_ALPHA = 0.1 ; 
+		dest = 1 - 0.1 = 0.9----> GL_ONE_MINUS_SRC_ALPHA = 0.9;
+		so the final color is
+			R = r_src * 0.1 + r_dest * (1- 0.1) -----> r_dest;
+			G = g_src * 0.1 + g_dest * (1- 0.1) -----> g_dest;
+			B = b_src * 0.1 + b_dest * (1- 0.1) -----> b_dest;
+			A = a_src * 0.1 + a_dest * (1- 0.1) -----> a_dest;
+			*/
+		GLCallV(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)); 
 
 		VertexArray va;
 		va.Bind();
